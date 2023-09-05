e86b39a61d93e7f2a26ec7833c44a841f8a20ac4
seyhani <aseyhani@icloud.com>
2023-09-05 20:04:52 +0330

    Remove random seed initialization from time

--- src/shared_config.cpp
+++ src/shared_config.cpp
@@ -10,9 +10,7 @@
 
 /******************************* Dists *****************************/
 Dists::Dists(DistsParams params) {
-    unsigned int seed = (unsigned int) time(NULL);
-    // unsigned int seed = 1680979592;
-    seed = params.random_seed;
+    unsigned int seed = params.random_seed;
     std::cout << "seed is: " << seed << std::endl;
     gen.seed(seed);
 

5c6cfa05ae239efbca154257399b2ecffc99bff3
seyhani <aseyhani@icloud.com>
2023-10-13 21:54:23 +0330

    Set random seed for prio

--- src/tests.cpp
+++ src/tests.cpp
@@ -72,6 +72,7 @@ void prio(std::string good_examples_file,
     dists_params.total_time = total_time;
     dists_params.pkt_meta1_val_max = 2;
     dists_params.pkt_meta2_val_max = 2;
+    dists_params.random_seed = 2000;
     
     Dists* dists = new Dists(dists_params); 
     SharedConfig* config = new SharedConfig(total_time,

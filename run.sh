b6e090eade60b9257b1bec43fbaf79e0efe1517a
seyhani <aseyhani@icloud.com>
2025-10-04 12:56:42 -0400

    use parallel

--- run.sh
+++ run.sh
@@ -1,16 +1,27 @@
+#!/usr/bin/env bash
 set -e
 trap "echo 'Interrupted!'; exit 1" INT
 
-RAND_SEED=$1
+SIZES=(10 20 25 30 40 50 75 100 125 150 175 200 250 300 350 400 450 500)
 
-mkdir -p "wls"
-mkdir -p "logs"
+TC=$1
+RAND_SEED=8000
+NPROC=${#SIZES[@]}
 
-for i in 10 20 25 30 40 50 75 100 125 150 175 200 250 300 350 400 450 500; do
-#for i in 40 50 75 100 125 150 175 200 250 300 350 400 450 500; do
+mkdir -p "wls/$TC"
+mkdir -p "logs/$TC"
+
+
+run_task() {
+  i=$1
   echo "RAND_SEED=$RAND_SEED"
   echo "BUF_SIZE=$i Started"
-  export WL_FILE="wls/fq.$i.txt"
-  ./build/fperf $i $RAND_SEED > "logs/fq.$i.log"
+  export WL_FILE="wls/$TC/$i.txt"
+  ./build/fperf "$i" "$RAND_SEED" > "logs/$TC/$i.log"
   echo "BUF_SIZE=$i Done"
-done
+}
+
+export -f run_task
+export RAND_SEED
+
+parallel -j "$NPROC" run_task ::: "${SIZES[@]}"
